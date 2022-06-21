8824a9e5c1b44147430a090d905ffd13c2970f3c
halim777 <halimzaafane@gmail.com>
2022-06-21 16:55:16 +0200

    halim777

---
+++ objet-etudiant.js
@@ -0,0 +1,32 @@
+function makeUser (name, firstname, statut) {
+    return {
+        name: name,
+        firstname: firstname,
+        statut: statut
+
+    };
+}
+
+let john = makeUser("John", "doe");
+let pierre = makeUser("pierre", "doe");
+
+}
+console.log(pierre.name); // Johnalert(user.name); // John
+// tester localstorage
+document.write(pierre.name);
+
+// try {
+//     localStorage.setItem("key", "value");
+// } catch (e) { console.log(e) }
+
+localStorage.clear();
+localStorage.setItem("key", JSON.stringify(pierre));
+
+let str = localStorage.getItem("key");
+console.log(str);
+
+
+//var x = new Boolean(true);
+// if (x) {
+//     console.log("true")
+// } else { console.log("false"); }
--- tableau.js
+++
@@ -1,2 +0,0 @@
-const prenom = [["pierre",10],["tom",0],["jules",15]];
-console.log(prenom[2])

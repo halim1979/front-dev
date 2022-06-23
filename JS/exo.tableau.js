9d5424c9cacb4adb2846189341c3e1146203209f
halim777 <halimzaafane59@gmail.com>
2022-06-23 11:28:56 +0200

    exomaison2306

---
+++ JS/exo.tableau.js
@@ -0,0 +1,10 @@
+let tableau = [];
+tableau[0]="Halim";
+tableau[1]= "Greg";
+tableau[2]="Fred";
+tableau[3]="Jimmy";
+tableau[4]="Othman";
+
+//console.table("Halim", "Fred", "Greg","Stephane", "Jimmy")
+console.log(tableau);
+//document

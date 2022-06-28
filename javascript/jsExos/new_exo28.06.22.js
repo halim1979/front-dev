d40ebde1efb61d098853f7265a5284d38fae93af
halim <halimzaafane@gmail.com>
2022-06-28 16:12:38 +0200

    exo28.06.22-16h

---
+++ javascript/jsExos/new_exo28.06.22.js
@@ -0,0 +1,22 @@
+
+/*function affichage(){
+  let data = 16;
+  function affichageInter(){
+    document.write(data)
+  }
+
+  affichageInter();
+}
+affichage()*/
+
+
+
+/*let plus = (a, b) => {return a%b}
+document.write(plus(8, 6));*/
+
+let i =0;
+while (i<5) {
+  document.write("aaa-")
+  i++;
+
+}
